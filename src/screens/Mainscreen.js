a6c3a8df4fb29ae8c899e59574b398d370138740
Ramin Radmand <69999501+imraminradmand@users.noreply.github.com>
2023-02-10 09:59:26 -0700

    start of home

---
+++ src/screens/Mainscreen.js
@@ -0,0 +1,16 @@
+import React from "react";
+import Searchbar from "../components/Searchbar";
+
+const Mainscreen = () => {
+  return (
+    <div className="bg-hero bg-center h-screen">
+      <div className="flex items-center justify-center flex-col">
+        <div>
+          <h1 className="font-sans text-7xl">Movie Browser</h1>
+        </div>
+        <Searchbar />
+      </div>
+    </div>
+  );
+};
+export default Mainscreen;
