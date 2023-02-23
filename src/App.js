e1ad8254481544c25254f8515660268a2a2230ad
Ramin Radmand <69999501+imraminradmand@users.noreply.github.com>
2023-02-23 10:35:16 -0700

    adding routing

--- src/App.js
+++ src/App.js
@@ -1,31 +1,36 @@
-import React, { useEffect } from "react";
-import "./App.css";
-import SingleMovie from "./components/SingleMovie";
-import Mainscreen from "./screens/Mainscreen";
-import Listscreen from "./screens/Listscreen";
 import { library } from "@fortawesome/fontawesome-svg-core";
-import { faCheckSquare, faCoffee } from "@fortawesome/free-solid-svg-icons";
 import { faHeart } from "@fortawesome/free-regular-svg-icons";
+import { faCheckSquare, faCoffee } from "@fortawesome/free-solid-svg-icons";
+import React, { useEffect } from "react";
+import { Route, Routes } from "react-router-dom";
+import "./App.css";
+import Mainscreen from "./screens/Mainscreen";
 library.add(faCheckSquare, faCoffee, faHeart);
 
 function App() {
-	useEffect(() => {
-		const getData = async () => {
-			try {
-				const url =
-					"https://www.randyconnolly.com/funwebdev/3rd/api/movie/movies-brief.php?limit=2";
-				const response = await fetch(url);
-				const data = await response.json();
-				localStorage.setItem("movies", JSON.stringify(data));
-			} catch (err) {
-				console.error(err);
-			}
-		};
-		// invoke the async function
-		getData();
-	}, []);
+  useEffect(() => {
+    const getData = async () => {
+      try {
+        const url =
+          "https://www.randyconnolly.com/funwebdev/3rd/api/movie/movies-brief.php?limit=2";
+        const response = await fetch(url);
+        const data = await response.json();
+        localStorage.setItem("movies", JSON.stringify(data));
+      } catch (err) {
+        console.error(err);
+      }
+    };
+    // invoke the async function
+    getData();
+  }, []);
 
-	return <SingleMovie />;
+  return (
+    <main>
+      <Routes>
+        <Route exact path="/" element={<Mainscreen />} />
+      </Routes>
+    </main>
+  );
 }
 
 export default App;
