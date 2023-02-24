9a2de943e379ceb3f2dceb903441d14ad8781bd5
Ramin Radmand <69999501+imraminradmand@users.noreply.github.com>
2023-02-23 17:13:20 -0700

    added filters

--- src/components/MovieFilter.js
+++ src/components/MovieFilter.js
@@ -1,24 +1,28 @@
 import React from "react";
-
+import Slider from "./Slider";
 
 const MovieFilter = (props) => {
-
   if (props.viewFilter === true) {
     return (
       <div className=" bg-secondaryGreen w-1/4 m-1 h-5/6">
-        <button onClick={props.hideFilHandler} className=" float-right h-full bg-primaryYellow hover:bg-lightGrey w-7">
+        <button
+          onClick={props.hideFilHandler}
+          className=" float-right h-full bg-primaryYellow hover:bg-lightGrey w-7"
+        >
           <div className=" mr-0"> ^ </div>
         </button>
-        Movie Filter
+        <Slider />
       </div>
     );
   } else {
     return (
       <div className=" bg-secondaryGreen w-0 m-1 mr-0 h-5/6">
-        <button onClick={props.viewFilHandler} className=" float-right h-full bg-primaryYellow hover:bg-lightGrey w-7">
+        <button
+          onClick={props.viewFilHandler}
+          className=" float-right h-full bg-primaryYellow hover:bg-lightGrey w-7"
+        >
           <div className=" ml-0"> v </div>
         </button>
-
       </div>
     );
   }
