99bdbdb2aa3c8688eeb2c271d379dc6af58f82bf
Ramin Radmand <69999501+imraminradmand@users.noreply.github.com>
2023-02-26 00:24:38 -0700

    sorting for title and year working

--- src/components/MovieList.js
+++ src/components/MovieList.js
@@ -1,48 +1,80 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import MovieCard from "./MovieCard";
 import MovieListHeader from "./MovieListHeader";
 
-const MovieList = (props) => {
-  if (props.viewFav === true && props.viewFilter === true) {
+const MovieList = ({ moviesToShow, viewFav, viewFilter, setFav }) => {
+  const [movieList, setMovieList] = useState(moviesToShow);
+  const [sortKey, setSortKey] = useState("");
+  const [sortOrder, setSortOrder] = useState("asc");
+
+  useEffect(() => {
+    setMovieList(moviesToShow);
+    if (sortKey !== "") {
+      const sortedMovies = movieList.sort((a, b) => {
+        if (sortOrder === "asc") {
+          return a[sortKey] > b[sortKey] ? 1 : -1;
+        } else if (sortOrder === "asc" && sortKey === "ratings") {
+          console.log(a[sortKey]["average"], b[sortKey]["average"]);
+          return a[sortKey]["average"] < b[sortKey]["average"] ? 1 : -1;
+        } else if (sortOrder === "asc" && sortKey === "popularity") {
+          return a[sortKey]["popularity"] < b[sortKey]["popularity"] ? 1 : -1;
+        } else {
+          return a[sortKey] < b[sortKey] ? 1 : -1;
+        }
+      });
+      setMovieList(sortedMovies);
+    }
+  }, [sortKey, sortOrder, movieList, moviesToShow]);
+
+  const handleSort = (key) => {
+    if (key === sortKey) {
+      setSortOrder(sortOrder === "asc" ? "desc" : "asc");
+    } else {
+      setSortKey(key);
+      setSortOrder("asc");
+    }
+  };
+
+  if (viewFav === true && viewFilter === true) {
     return (
       <div className=" bg-secondaryGreen m-1 p-1 h-5/6 w-1/2 rounded-md">
-        <MovieListHeader />
+        <MovieListHeader sortHandler={handleSort} />
         <div className="bg-secondaryGreen h-5/6 overflow-auto rounded-md mt-3">
-          {props.moviesToShow.map((movie, i) => {
-            return <MovieCard key={i} movie={movie} setFav={props.setFav} />;
+          {movieList.map((movie, i) => {
+            return <MovieCard key={i} movie={movie} setFav={setFav} />;
           })}
         </div>
       </div>
     );
-  } else if (props.viewFav === false && props.viewFilter === true) {
+  } else if (viewFav === false && viewFilter === true) {
     return (
       <div className=" bg-secondaryGreen m-1 p-1 h-5/6 w-8/12 rounded-md">
         <div className="bg-secondaryGreen h-5/6 overflow-auto rounded-md mt-3">
-					{props.moviesToShow.map((movie, i) => {
-						return <MovieCard key={i} movie={movie} />;
-					})}
-				</div>
+          {movieList.map((movie, i) => {
+            return <MovieCard key={i} movie={movie} />;
+          })}
+        </div>
       </div>
     );
-  } else if (props.viewFav === true && props.viewFilter === false) {
+  } else if (viewFav === true && viewFilter === false) {
     return (
       <div className=" bg-secondaryGreen m-1 p-1 h-5/6 w-9/12 rounded-md">
-        				<div className="bg-secondaryGreen h-5/6 overflow-auto rounded-md mt-3">
-					{props.moviesToShow.map((movie, i) => {
-						return <MovieCard key={i} movie={movie} />;
-					})}
-				</div>
+        <div className="bg-secondaryGreen h-5/6 overflow-auto rounded-md mt-3">
+          {movieList.map((movie, i) => {
+            return <MovieCard key={i} movie={movie} />;
+          })}
+        </div>
       </div>
     );
   } else {
-    props.moviesToShow.forEach((movie) => {
+    moviesToShow.forEach((movie) => {
       return (
         <div className=" bg-secondaryGreen m-1 p-1 h-5/6 w-11/12 rounded-md">
           <div className="bg-secondaryGreen h-5/6 overflow-auto rounded-md mt-3">
-					{props.moviesToShow.map((movie, i) => {
-						return <MovieCard key={i} movie={movie} />;
-					})}
-				</div>
+            {movieList.map((movie, i) => {
+              return <MovieCard key={i} movie={movie} />;
+            })}
+          </div>
         </div>
       );
     });
--- src/components/MovieListHeader.js
+++ src/components/MovieListHeader.js
@@ -1,25 +1,42 @@
-import React from "react";
+import { faSort } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import React from "react";
 
-const MovieListHeader = () => {
-	return (
-		<div className="grid grid-cols-7 gap-4 px-4 py-2 border-b border-gray-200">
-			<div className="col-span-1"></div>
-			<div className="text-lg font-medium text-center">
-				Title <FontAwesomeIcon icon="arrows-up-down" size="2xs" />
-			</div>
-			<div className="text-lg font-medium text-center">
-				Year <FontAwesomeIcon icon="arrows-up-down" size="2xs" />
-			</div>
-			<div className="text-lg font-medium text-center">
-				Rating <FontAwesomeIcon icon="arrows-up-down" size="2xs" />
-			</div>
-			<div className="text-lg font-medium text-center">
-				Popularity <FontAwesomeIcon icon="arrows-up-down" size="2xs" />
-			</div>
-			<div className="col-span-2"></div>
-		</div>
-	);
+const MovieListHeader = ({ sortHandler }) => {
+  const handleSort = (key) => {
+    sortHandler(key);
+  };
+
+  return (
+    <div className="grid grid-cols-7 gap-4 px-4 py-2 border-b border-gray-200">
+      <div className="col-span-1"></div>
+      <div
+        className="text-lg font-medium text-center cursor-pointer"
+        onClick={() => handleSort("title")}
+      >
+        Title <FontAwesomeIcon icon={faSort} size="2xs" />
+      </div>
+      <div
+        className="text-lg font-medium text-center cursor-pointer"
+        onClick={() => handleSort("release_date")}
+      >
+        Year <FontAwesomeIcon icon={faSort} size="2xs" />
+      </div>
+      <div
+        className="text-lg font-medium text-center cursor-pointer"
+        onClick={() => handleSort("ratings")}
+      >
+        Rating <FontAwesomeIcon icon={faSort} size="2xs" />
+      </div>
+      <div
+        className="text-lg font-medium text-center cursor-pointer"
+        onClick={() => handleSort("popularity")}
+      >
+        Popularity <FontAwesomeIcon icon={faSort} size="2xs" />
+      </div>
+      <div className="col-span-2"></div>
+    </div>
+  );
 };
 
 export default MovieListHeader;
