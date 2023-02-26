b887576a7016c8a114293de3a352661e2e074591
Ramin Radmand <69999501+imraminradmand@users.noreply.github.com>
2023-02-25 22:44:22 -0700

    checking intially if localStorage has movies

--- src/App.js
+++ src/App.js
@@ -38,14 +38,17 @@ function App() {
 
   useEffect(() => {
     const getData = async () => {
-      try {
-        const url =
-          "https://www.randyconnolly.com/funwebdev/3rd/api/movie/movies-brief.php?limit=10";
-        const response = await fetch(url);
-        const data = await response.json();
-        localStorage.setItem("movies", JSON.stringify(data));
-      } catch (err) {
-        console.error(err);
+      // check if local storage is empty is so fetch data
+      if (localStorage.getItem("movies") === null) {
+        try {
+          const url =
+            "https://www.randyconnolly.com/funwebdev/3rd/api/movie/movies-brief.php?limit=10";
+          const response = await fetch(url);
+          const data = await response.json();
+          localStorage.setItem("movies", JSON.stringify(data));
+        } catch (err) {
+          console.error(err);
+        }
       }
     };
     // invoke the async function
