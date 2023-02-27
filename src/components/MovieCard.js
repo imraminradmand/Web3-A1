02fdd59514ba3688b3dad5d61fa60b516c5a83ee
bbode837 <bbode837@mtroyal.ca>
2023-02-27 15:30:05 -0700

    movie list filter shows right movie

--- src/components/MovieCard.js
+++ src/components/MovieCard.js
@@ -3,76 +3,76 @@ import React, { useState } from "react";
 import { Link, useNavigate } from "react-router-dom";
 
 function MovieCard(props) {
-  const navigate = useNavigate();
-  const posterURL = `https://image.tmdb.org/t/p/w92/${props.movie.poster}`;
-  const [singleMovie, setSingleMovie] = useState(props.movie);
+	const navigate = useNavigate();
+	const posterURL = `https://image.tmdb.org/t/p/w92/${props.movie.poster}`;
+	// const [singleMovie, setSingleMovie] = useState(props.movie);
 
-  const testfav = true;
-  let statement = "";
+	const testfav = true;
+	let statement = "";
 
-  function isFavorite() {
-    if (testfav === true) {
-      statement = <FontAwesomeIcon icon={["fa", "heart"]} color="red" />;
-    } else {
-      statement = <FontAwesomeIcon icon={["far", "heart"]} />;
-    }
-    return statement;
-  }
+	function isFavorite() {
+		if (testfav === true) {
+			statement = <FontAwesomeIcon icon={["fa", "heart"]} color="red" />;
+		} else {
+			statement = <FontAwesomeIcon icon={["far", "heart"]} />;
+		}
+		return statement;
+	}
 
-  const navigateToMovie = () => {
-    navigate("/movie", { state: { singleMovie: singleMovie } });
-  };
+	const navigateToMovie = () => {
+		navigate("/movie", { state: { singleMovie: props.movie } });
+	};
 
-  const handleFavAdd = () => {
-    if (!props.favs.includes(props.movie)) {
-      props.setFav([...props.favs, props.movie]);
-    }
-  };
-  return (
-    <div className="grid grid-cols-7 gap-4 px-4 py-2 items-center bg-primaryGreen mb-1">
-      <div
-        className="h-40 col-span-1 flex justify-center cursor-pointer"
-        onClick={() => {
-          navigateToMovie();
-        }}
-      >
-        <img
-          src={posterURL}
-          className="h-full max-w-full"
-          alt={`${props.movie.title} poster`}
-        />
-      </div>
-      <div
-        className="h-12 col-span-1 text-center cursor-pointer"
-        onClick={() => {
-          navigateToMovie();
-        }}
-      >
-        {props.movie.title}
-      </div>
-      <div className="h-12 col-span-1 text-center">
-        {props.movie["release_date"].substr(0, 4)}
-      </div>
-      <div className="h-12 col-span-1 text-center">
-        {props.movie.ratings.average}
-      </div>
-      <div className="h-12 col-span-1 text-center">
-        {props.movie.ratings.popularity.toFixed(1)}
-      </div>
-      <div className="h-12 col-span-1 text-center">
-        <button className="" onClick={() => handleFavAdd()}>
-          {isFavorite()}
-        </button>
-      </div>
-      <div className="col-span-1 text-center">
-        <Link to="/movie" state={{ singleMovie: singleMovie }}>
-          <button className="m-2 bg-primaryOrange border-secondaryGreen hover:border-primaryOrange border-solid border-2 hover:bg-secondaryGreen pl-3 pr-3 pt-2 pb-2 text-center rounded">
-            Show
-          </button>
-        </Link>
-      </div>
-    </div>
-  );
+	const handleFavAdd = () => {
+		if (!props.favs.includes(props.movie)) {
+			props.setFav([...props.favs, props.movie]);
+		}
+	};
+	return (
+		<div className="grid grid-cols-7 gap-4 px-4 py-2 items-center bg-primaryGreen mb-1">
+			<div
+				className="h-40 col-span-1 flex justify-center cursor-pointer"
+				onClick={() => {
+					navigateToMovie();
+				}}
+			>
+				<img
+					src={posterURL}
+					className="h-full max-w-full"
+					alt={`${props.movie.title} poster`}
+				/>
+			</div>
+			<div
+				className="h-12 col-span-1 text-center cursor-pointer"
+				onClick={() => {
+					navigateToMovie();
+				}}
+			>
+				{props.movie.title}
+			</div>
+			<div className="h-12 col-span-1 text-center">
+				{props.movie["release_date"].substr(0, 4)}
+			</div>
+			<div className="h-12 col-span-1 text-center">
+				{props.movie.ratings.average}
+			</div>
+			<div className="h-12 col-span-1 text-center">
+				{props.movie.ratings.popularity.toFixed(1)}
+			</div>
+			<div className="h-12 col-span-1 text-center">
+				<button className="" onClick={() => handleFavAdd()}>
+					{isFavorite()}
+				</button>
+			</div>
+			<div className="col-span-1 text-center">
+				<Link to="/movie" state={{ singleMovie: props.movie }}>
+					<button className="m-2 bg-primaryOrange border-secondaryGreen hover:border-primaryOrange border-solid border-2 hover:bg-secondaryGreen pl-3 pr-3 pt-2 pb-2 text-center rounded">
+						Show
+					</button>
+				</Link>
+			</div>
+		</div>
+	);
 }
 
 export default MovieCard;
