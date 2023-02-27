4e39b15f8e6c535c6bace2548554f9f172f4c462
Ramin Radmand <69999501+imraminradmand@users.noreply.github.com>
2023-02-26 17:12:33 -0700

    no movies to show

--- src/components/MovieList.js
+++ src/components/MovieList.js
@@ -3,105 +3,99 @@ import MovieCard from "./MovieCard";
 import MovieListHeader from "./MovieListHeader";
 
 const MovieList = ({ moviesToShow, viewFav, viewFilter, favs, setFav }) => {
-	const [movieList, setMovieList] = useState(moviesToShow);
-	const [sortKey, setSortKey] = useState("");
-	const [sortOrder, setSortOrder] = useState("asc");
+  const [movieList, setMovieList] = useState(moviesToShow);
+  const [sortKey, setSortKey] = useState("");
+  const [sortOrder, setSortOrder] = useState("asc");
 
-	useEffect(() => {
-		setMovieList(moviesToShow);
-		if (sortKey !== "") {
-			const sortedMovies = movieList.sort((a, b) => {
-				if (sortOrder === "asc") {
-					if (sortKey === "ratings") {
-						return a[sortKey]["average"] > b[sortKey]["average"] ? 1 : -1;
-					} else if (sortKey === "popularity") {
-						return a["ratings"]["popularity"] > b["ratings"]["popularity"]
-							? 1
-							: -1;
-					} else {
-						return a[sortKey] > b[sortKey] ? 1 : -1;
-					}
-				} else {
-					if (sortKey === "ratings") {
-						return a[sortKey]["average"] < b[sortKey]["average"] ? 1 : -1;
-					} else if (sortKey === "popularity") {
-						return a["ratings"]["popularity"] < b["ratings"]["popularity"]
-							? 1
-							: -1;
-					} else {
-						return a[sortKey] < b[sortKey] ? 1 : -1;
-					}
-				}
-			});
-			setMovieList(sortedMovies);
-		}
-	}, [sortKey, sortOrder, movieList, moviesToShow]);
+  useEffect(() => {
+    setMovieList(moviesToShow);
+    if (sortKey !== "") {
+      const sortedMovies = movieList.sort((a, b) => {
+        if (sortOrder === "asc") {
+          if (sortKey === "ratings") {
+            return a[sortKey]["average"] > b[sortKey]["average"] ? 1 : -1;
+          } else if (sortKey === "popularity") {
+            return a["ratings"]["popularity"] > b["ratings"]["popularity"]
+              ? 1
+              : -1;
+          } else {
+            return a[sortKey] > b[sortKey] ? 1 : -1;
+          }
+        } else {
+          if (sortKey === "ratings") {
+            return a[sortKey]["average"] < b[sortKey]["average"] ? 1 : -1;
+          } else if (sortKey === "popularity") {
+            return a["ratings"]["popularity"] < b["ratings"]["popularity"]
+              ? 1
+              : -1;
+          } else {
+            return a[sortKey] < b[sortKey] ? 1 : -1;
+          }
+        }
+      });
+      setMovieList(sortedMovies);
+    }
+  }, [sortKey, sortOrder, movieList, moviesToShow]);
 
-	const handleSort = (key) => {
-		if (key === sortKey) {
-			setSortOrder(sortOrder === "asc" ? "desc" : "asc");
-		} else {
-			setSortKey(key);
-			setSortOrder("asc");
-		}
-	};
+  const handleSort = (key) => {
+    if (key === sortKey) {
+      setSortOrder(sortOrder === "asc" ? "desc" : "asc");
+    } else {
+      setSortKey(key);
+      setSortOrder("asc");
+    }
+  };
 
-	// function isEmpty(movieList) {
-	// 	if (movieList.length === 0) {
-	// 		return <h1 className="text-white text-2xl">No movies found</h1>;
-	// 	}
-	// 	movieList.map((movie, i) => {
-	// 		return <MovieCard key={i} movie={movie} setFav={setFav} favs={favs} />;
-	// 	});
-	// }
-
-	if (viewFav === true && viewFilter === true) {
-		return (
-			<div className=" bg-secondaryGreen m-1 p-1 h-5/6 w-1/2 rounded-md">
-				<MovieListHeader sortHandler={handleSort} />
-				<div className="bg-secondaryGreen h-5/6 overflow-auto rounded-md mt-3">
-					{/* {isEmpty(movieList)} */}
-					{movieList.map((movie, i) => {
-						return (
-							<MovieCard key={i} movie={movie} setFav={setFav} favs={favs} />
-						);
-					})}
-				</div>
-			</div>
-		);
-	} else if (viewFav === false && viewFilter === true) {
-		return (
-			<div className=" bg-secondaryGreen m-1 p-1 h-5/6 w-8/12 rounded-md">
-				<MovieListHeader sortHandler={handleSort} />
-				<div className="bg-secondaryGreen h-5/6 overflow-auto rounded-md mt-3">
-					{movieList.map((movie, i) => {
-						return <MovieCard key={i} movie={movie} />;
-					})}
-				</div>
-			</div>
-		);
-	} else if (viewFav === true && viewFilter === false) {
-		return (
-			<div className=" bg-secondaryGreen m-1 p-1 h-5/6 w-9/12 rounded-md">
-				<MovieListHeader sortHandler={handleSort} />
-				<div className="bg-secondaryGreen h-5/6 overflow-auto rounded-md mt-3">
-					{movieList.map((movie, i) => {
-						return <MovieCard key={i} movie={movie} />;
-					})}
-				</div>
-			</div>
-		);
-	} else {
-		return (
-			<div className=" bg-secondaryGreen m-1 p-1 h-5/6 w-11/12 rounded-md">
-				<MovieListHeader sortHandler={handleSort} />
-				<div className="bg-secondaryGreen h-5/6 overflow-auto rounded-md mt-3">
-					{movieList.map((movie, i) => {
-						return <MovieCard key={i} movie={movie} />;
-					})}
-				</div>
-			</div>
-		);
-	}
+  if (viewFav === true && viewFilter === true) {
+    return (
+      <div className=" bg-secondaryGreen m-1 p-1 h-5/6 w-1/2 rounded-md">
+        <MovieListHeader sortHandler={handleSort} />
+        <div className="bg-secondaryGreen h-5/6 overflow-auto rounded-md mt-3">
+          {movieList.length === 0 ? (
+            <div className="flex justify-center items-center h-full">
+              <h1 className="text-white text-2xl font-bold">No movies found</h1>
+            </div>
+          ) : (
+            movieList.map((movie, i) => (
+              <MovieCard key={i} movie={movie} setFav={setFav} favs={favs} />
+            ))
+          )}
+        </div>
+      </div>
+    );
+  } else if (viewFav === false && viewFilter === true) {
+    return (
+      <div className=" bg-secondaryGreen m-1 p-1 h-5/6 w-8/12 rounded-md">
+        <MovieListHeader sortHandler={handleSort} />
+        <div className="bg-secondaryGreen h-5/6 overflow-auto rounded-md mt-3">
+          {movieList.map((movie, i) => {
+            return <MovieCard key={i} movie={movie} />;
+          })}
+        </div>
+      </div>
+    );
+  } else if (viewFav === true && viewFilter === false) {
+    return (
+      <div className=" bg-secondaryGreen m-1 p-1 h-5/6 w-9/12 rounded-md">
+        <MovieListHeader sortHandler={handleSort} />
+        <div className="bg-secondaryGreen h-5/6 overflow-auto rounded-md mt-3">
+          {movieList.map((movie, i) => {
+            return <MovieCard key={i} movie={movie} />;
+          })}
+        </div>
+      </div>
+    );
+  } else {
+    return (
+      <div className=" bg-secondaryGreen m-1 p-1 h-5/6 w-11/12 rounded-md">
+        <MovieListHeader sortHandler={handleSort} />
+        <div className="bg-secondaryGreen h-5/6 overflow-auto rounded-md mt-3">
+          {movieList.map((movie, i) => {
+            return <MovieCard key={i} movie={movie} />;
+          })}
+        </div>
+      </div>
+    );
+  }
 };
 export default MovieList;
--- src/screens/Listscreen.js
+++ src/screens/Listscreen.js
@@ -6,187 +6,185 @@ import MovieFilter from "../components/MovieFilter";
 import MovieList from "../components/MovieList";
 
 const Listscreen = (props) => {
-	const navigate = useNavigate();
-	const [movies, setMovies] = useState(
-		JSON.parse(localStorage.getItem("movies"))
-	);
-	const [requestMovie, setRequestedMovie] = useState([]);
+  const navigate = useNavigate();
+  const [movies, setMovies] = useState(
+    JSON.parse(localStorage.getItem("movies"))
+  );
+  const [requestMovie, setRequestedMovie] = useState([]);
 
-	const { filteredList, setFilteredList } = useState([]);
+  const { filteredList, setFilteredList } = useState([]);
 
-	const [titleInput, setTitleInput] = useState("");
-	const [yearInputLower, setYearInputLower] = useState("");
-	const [yearInputUpper, setYearInputUpper] = useState("");
-	const [ratingInputLower, setRatingInputLower] = useState("");
-	const [ratingInputUpper, setRatingInputUpper] = useState("");
-	const [genreInput, setGenreInput] = useState("");
+  const [titleInput, setTitleInput] = useState("");
+  const [yearInputLower, setYearInputLower] = useState("");
+  const [yearInputUpper, setYearInputUpper] = useState("");
+  const [ratingInputLower, setRatingInputLower] = useState("");
+  const [ratingInputUpper, setRatingInputUpper] = useState("");
+  const [genreInput, setGenreInput] = useState("");
 
-	const [viewFavourite, setViewFavourite] = useState(true);
-	const [viewFilter, setViewFilter] = useState(true);
+  const [viewFavourite, setViewFavourite] = useState(true);
+  const [viewFilter, setViewFilter] = useState(true);
 
-	function newFilterResults(filteredList) {
-		setRequestedMovie(filteredList);
-		console.log(filteredList);
-	}
+  function newFilterResults(filteredList) {
+    setRequestedMovie(filteredList);
+  }
 
-	useEffect(() => {
-		if (!props.searchedMovie) {
-			navigate("/");
-		}
-		if (props.searchedMovie === " ") {
-			const sortedMovies = movies
-				.slice()
-				.sort((a, b) => a.title.localeCompare(b.title));
-			setRequestedMovie(sortedMovies);
-		} else {
-			const filteredMovie = movies.filter((movie) =>
-				movie.title.toLowerCase().includes(props.searchedMovie)
-			);
-			const sortedMovies = filteredMovie
-				.slice()
-				.sort((a, b) => a.title.localeCompare(b.title));
-			setRequestedMovie(sortedMovies);
-		}
-	}, [props.searchedMovie, movies, navigate]);
+  useEffect(() => {
+    if (!props.searchedMovie) {
+      navigate("/");
+    }
+    if (props.searchedMovie === " ") {
+      const sortedMovies = movies
+        .slice()
+        .sort((a, b) => a.title.localeCompare(b.title));
+      setRequestedMovie(sortedMovies);
+    } else {
+      const filteredMovie = movies.filter((movie) =>
+        movie.title.toLowerCase().includes(props.searchedMovie)
+      );
+      const sortedMovies = filteredMovie
+        .slice()
+        .sort((a, b) => a.title.localeCompare(b.title));
+      setRequestedMovie(sortedMovies);
+    }
+  }, [props.searchedMovie, movies, navigate]);
 
-	const viewHandler = () => {
-		setViewFavourite(true);
-	};
+  const viewHandler = () => {
+    setViewFavourite(true);
+  };
 
-	const hideHandler = () => {
-		setViewFavourite(false);
-	};
+  const hideHandler = () => {
+    setViewFavourite(false);
+  };
 
-	const viewFilHandler = () => {
-		setViewFilter(true);
-	};
+  const viewFilHandler = () => {
+    setViewFilter(true);
+  };
 
-	const hideFilHandler = () => {
-		setViewFilter(false);
-	};
+  const hideFilHandler = () => {
+    setViewFilter(false);
+  };
 
-	const handleFiltering = (
-		title,
-		genre,
-		lowerYear,
-		upperYear,
-		lowerRating,
-		upperRating
-	) => {
-		setTitleInput(title);
-		setYearInputLower(lowerYear);
-		setYearInputUpper(upperYear);
-		setRatingInputLower(lowerRating);
-		setRatingInputUpper(upperRating);
-		setGenreInput(genre);
-		console.log(title, genre, lowerYear, upperYear, lowerRating, upperRating);
-	};
+  const handleFiltering = (
+    title,
+    genre,
+    lowerYear,
+    upperYear,
+    lowerRating,
+    upperRating
+  ) => {
+    setTitleInput(title);
+    setYearInputLower(lowerYear);
+    setYearInputUpper(upperYear);
+    setRatingInputLower(lowerRating);
+    setRatingInputUpper(upperRating);
+    setGenreInput(genre);
+  };
 
-	if (viewFavourite === false && viewFilter === true) {
-		return (
-			<div className=" bg-primaryGreen h-screen flex flex-wrap justify-center">
-				<Header />
-				<MovieFilter
-					hideFilHandler={hideFilHandler}
-					viewFilter={viewFilter}
-					filtering={handleFiltering}
-					movies={movies}
-					newFilterResults={newFilterResults}
-				/>
-				<MovieList
-					viewFav={viewFavourite}
-					viewFilter={viewFilter}
-					moviesToShow={requestMovie}
-					favs={props.favMovie}
-					setFav={props.setFavs}
-				/>
+  if (viewFavourite === false && viewFilter === true) {
+    return (
+      <div className=" bg-primaryGreen h-screen flex flex-wrap justify-center">
+        <Header />
+        <MovieFilter
+          hideFilHandler={hideFilHandler}
+          viewFilter={viewFilter}
+          filtering={handleFiltering}
+          movies={movies}
+          newFilterResults={newFilterResults}
+        />
+        <MovieList
+          viewFav={viewFavourite}
+          viewFilter={viewFilter}
+          moviesToShow={requestMovie}
+          favs={props.favMovie}
+          setFav={props.setFavs}
+        />
 
-				<FavouriteList
-					viewHandler={viewHandler}
-					viewFav={viewFavourite}
-					favs={props.favMovie}
-					setFav={props.setFavs}
-				/>
-			</div>
-		);
-	} else if (viewFavourite === true && viewFilter === false) {
-		return (
-			<div className=" bg-primaryGreen h-screen flex flex-wrap justify-center">
-				<Header />
-				<MovieFilter
-					viewFilHandler={viewFilHandler}
-					viewFilter={viewFilter}
-					filtering={handleFiltering}
-					movies={movies}
-					newFilterResults={newFilterResults}
-				/>
-				<MovieList
-					viewFav={viewFavourite}
-					viewFilter={viewFilter}
-					moviesToShow={requestMovie}
-					favs={props.favMovie}
-					setFav={props.setFavs}
-				/>
-				<FavouriteList
-					hideHandler={hideHandler}
-					viewFav={viewFavourite}
-					favs={props.favMovie}
-					setFav={props.setFavs}
-				/>
-			</div>
-		);
-	} else if (viewFavourite === false && viewFilter === false) {
-		return (
-			<div className=" bg-primaryGreen h-screen flex flex-wrap justify-center">
-				<Header />
-				<MovieFilter
-					viewFilHandler={viewFilHandler}
-					viewFilter={viewFilter}
-					filtering={handleFiltering}
-					movies={movies}
-					newFilterResults={newFilterResults}
-				/>
-				<MovieList
-					viewFav={viewFavourite}
-					viewFilter={viewFilter}
-					moviesToShow={requestMovie}
-					favs={props.favMovie}
-					setFav={props.setFavs}
-				/>
-				<FavouriteList
-					viewHandler={viewHandler}
-					viewFav={viewFavourite}
-					favs={props.favMovie}
-					setFav={props.setFavs}
-				/>
-			</div>
-		);
-	} else {
-		return (
-			<div className=" bg-primaryGreen h-screen flex flex-wrap justify-center">
-				<Header />
-				<MovieFilter
-					hideFilHandler={hideFilHandler}
-					viewFilter={viewFilter}
-					filtering={handleFiltering}
-					movies={movies}
-					newFilterResults={newFilterResults}
-				/>
-				<MovieList
-					viewFav={viewFavourite}
-					viewFilter={viewFilter}
-					moviesToShow={requestMovie}
-					favs={props.favMovie}
-					setFav={props.setFavs}
-				/>
-				<FavouriteList
-					hideHandler={hideHandler}
-					viewFav={viewFavourite}
-					favs={props.favMovie}
-					setFav={props.setFavs}
-				/>
-			</div>
-		);
-	}
+        <FavouriteList
+          viewHandler={viewHandler}
+          viewFav={viewFavourite}
+          favs={props.favMovie}
+          setFav={props.setFavs}
+        />
+      </div>
+    );
+  } else if (viewFavourite === true && viewFilter === false) {
+    return (
+      <div className=" bg-primaryGreen h-screen flex flex-wrap justify-center">
+        <Header />
+        <MovieFilter
+          viewFilHandler={viewFilHandler}
+          viewFilter={viewFilter}
+          filtering={handleFiltering}
+          movies={movies}
+          newFilterResults={newFilterResults}
+        />
+        <MovieList
+          viewFav={viewFavourite}
+          viewFilter={viewFilter}
+          moviesToShow={requestMovie}
+          favs={props.favMovie}
+          setFav={props.setFavs}
+        />
+        <FavouriteList
+          hideHandler={hideHandler}
+          viewFav={viewFavourite}
+          favs={props.favMovie}
+          setFav={props.setFavs}
+        />
+      </div>
+    );
+  } else if (viewFavourite === false && viewFilter === false) {
+    return (
+      <div className=" bg-primaryGreen h-screen flex flex-wrap justify-center">
+        <Header />
+        <MovieFilter
+          viewFilHandler={viewFilHandler}
+          viewFilter={viewFilter}
+          filtering={handleFiltering}
+          movies={movies}
+          newFilterResults={newFilterResults}
+        />
+        <MovieList
+          viewFav={viewFavourite}
+          viewFilter={viewFilter}
+          moviesToShow={requestMovie}
+          favs={props.favMovie}
+          setFav={props.setFavs}
+        />
+        <FavouriteList
+          viewHandler={viewHandler}
+          viewFav={viewFavourite}
+          favs={props.favMovie}
+          setFav={props.setFavs}
+        />
+      </div>
+    );
+  } else {
+    return (
+      <div className=" bg-primaryGreen h-screen flex flex-wrap justify-center">
+        <Header />
+        <MovieFilter
+          hideFilHandler={hideFilHandler}
+          viewFilter={viewFilter}
+          filtering={handleFiltering}
+          movies={movies}
+          newFilterResults={newFilterResults}
+        />
+        <MovieList
+          viewFav={viewFavourite}
+          viewFilter={viewFilter}
+          moviesToShow={requestMovie}
+          favs={props.favMovie}
+          setFav={props.setFavs}
+        />
+        <FavouriteList
+          hideHandler={hideHandler}
+          viewFav={viewFavourite}
+          favs={props.favMovie}
+          setFav={props.setFavs}
+        />
+      </div>
+    );
+  }
 };
 export default Listscreen;
